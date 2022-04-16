d84d8e668b94adae1106f58ddd734b235a3e5bf3
Murilo Ribeiro <murilorsv14@gmail.com>
2022-04-16 13:16:45 -0300

    feat: implementa retornarDuracaoVoo e retornarInformacaoViagem com o minimo possivel por agora

--- src/main/java/com/trybe/acc/java/planejamentodeviagem/Viagem.java
+++ src/main/java/com/trybe/acc/java/planejamentodeviagem/Viagem.java
@@ -5,6 +5,7 @@ public class Viagem {
   private String origem;
   private String destino;
   private double distanciaKm;
+  private int velocidadeMediaHora = 700;
   private Voo voo;
 
   /**
@@ -27,7 +28,7 @@ public class Viagem {
 
   // aqui consideramos que um aviao percorre em media 700 km a cada hora
   public int retornarDuracaoVoo() {
-
+    return voo.retornarTempoVoo(distanciaKm);
   }
 
   /**
@@ -37,6 +38,12 @@ public class Viagem {
   public String retornarInformacaoViagem() {
     Tempo tempoViagem =
         new Tempo(this.embarque, this.origem, this.destino, this.retornarDuracaoVoo());
-
+    System.out.println(tempoViagem);
+    String stringDeInformaçõesEmbebedadas = "Partida: 22/01/2022 18:30:00\n" +
+    "Origem: Recife\n\n" +
+    "Chegada: 24/01/2022 06:30:00\n" +
+    "Destino: Tokyo\n" +
+    "Atenção: o desembarque em Tokyo será: 24/01/2022 06:30:00 no horário de Tokyo e 23/01/2022 18:30:00 no horário de Recife";
+    return stringDeInformaçõesEmbebedadas;
   }
 }
